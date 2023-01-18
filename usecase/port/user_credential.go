9d8196ba42ecc16b99cda052f93c4bed496a9441
mkaiho <mkaiho.cgengo@gmail.com>
2023-01-18 09:26:06 +0000

    modify user credential gateway interface

--- usecase/port/user_credential.go
+++ usecase/port/user_credential.go
@@ -1,8 +1,20 @@
 package port
 
-import "github.com/mkaiho/go-auth-api/entity"
+import (
+	"context"
 
-type UserCredentialRepository interface {
-	GetByEmail(email entity.Email) (*entity.UserCredential, error)
-	Create(email entity.Email, password entity.Password) error
+	"github.com/mkaiho/go-auth-api/entity"
+)
+
+type (
+	UserCredentialCreateInput struct {
+		UserID   entity.ID
+		Email    entity.Email
+		Password entity.Password
+	}
+)
+
+type UserCredentialGateway interface {
+	GetByEmail(ctx context.Context, email entity.Email) (*entity.UserCredential, error)
+	Create(ctx context.Context, input UserCredentialCreateInput) error
 }
